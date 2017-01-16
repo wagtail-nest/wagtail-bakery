36fa9ec4f1e647df068c9518daf849d03b02060f
Clinton Blackburn <clinton.blackburn@gmail.com>
2017-01-16 01:15:51 -0500

    Using a range for the django-bakery requirement
    This prevents the dependency from becoming stale when new updates are released; but avoids errors from a major version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 install_requires = [
-    'django-bakery==0.8.11',
+    'django-bakery>=0.8.12,<1.0.0',
 ]
 
 docs_require = [
@@ -21,7 +21,7 @@ test_require = [
 
 setup(
     name='wagtail-bakery',
-    version='0.1.0',
+    version='0.1.1',
     description='A set of helpers for baking your Django Wagtail site out as flat files.',
     author='Rob Moorman',
     author_email='rob@moori.nl',
