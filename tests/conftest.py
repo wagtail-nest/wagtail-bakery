000879903bf90cd0d116ddae089fb4c961187107
falseperfection <kevin@falseperfection.com>
2018-06-28 23:01:53 -0500

    match supported wagtail versions and update test settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -42,20 +42,20 @@ def pytest_configure():
 
             'tests',
         ],
-        MIDDLEWARE_CLASSES=[
+        MIDDLEWARE=[
             'django.middleware.security.SecurityMiddleware',
             'django.contrib.sessions.middleware.SessionMiddleware',
             'django.middleware.locale.LocaleMiddleware',
             'django.middleware.common.CommonMiddleware',
             'django.middleware.csrf.CsrfViewMiddleware',
             'django.contrib.auth.middleware.AuthenticationMiddleware',
-            'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
             'django.contrib.messages.middleware.MessageMiddleware',
             'django.middleware.clickjacking.XFrameOptionsMiddleware',
             'wagtail.core.middleware.SiteMiddleware',
             'wagtail.contrib.redirects.middleware.RedirectMiddleware',
         ],
         ROOT_URLCONF='tests.urls',
+        ALLOWED_HOSTS='*',
         TEMPLATES=[
             {
                 'BACKEND': 'django.template.backends.django.DjangoTemplates',
