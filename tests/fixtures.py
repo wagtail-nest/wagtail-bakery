cadd644090c13642ab4ac7afda24e37ceb3ba7f1
Loic Teixeira <hello@lta.me>
2019-11-11 10:14:45 +0100

    Fix DisallowedHost error
    > Invalid HTTP_HOST header: 'site_2'. The domain name provided is not valid according to RFC 1034/1035.

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -37,14 +37,14 @@ def multisite(site):
 
     page_2 = PageFactory(path='00010003', slug='page-2')
     site_2 = SiteFactory(
-        hostname='site_2', is_default_site=False, root_page=page_2)
+        hostname='site2', is_default_site=False, root_page=page_2)
 
     page_3 = PageFactory(path='00010004', slug='page-3')
     site_3 = SiteFactory(
-        hostname='site_3', is_default_site=False, root_page=page_3)
+        hostname='site3', is_default_site=False, root_page=page_3)
 
     page_4 = PageFactory(path='00010005', slug='page-4')
     site_4 = SiteFactory(
-        hostname='site_4', is_default_site=False, root_page=page_4)
+        hostname='site4', is_default_site=False, root_page=page_4)
 
     return [site, site_2, site_3, site_4]
