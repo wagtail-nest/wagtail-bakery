e718fe52bc0b9cfc756c6b85428eb64a8bd9268c
Loic Teixeira <hello@lta.me>
2020-07-19 13:13:16 +0200

    Fix Deprecation Warnings with Wagtail 2.8

--- src/wagtailbakery/api_views.py
+++ src/wagtailbakery/api_views.py
@@ -5,9 +5,10 @@ import os
 from bakery.views import BuildableMixin
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
-from wagtail.api.v2.endpoints import PagesAPIEndpoint
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.core.models import Page, Site
+
+from wagtailbakery.compatibility import PagesAPIViewSet
 
 logger = logging.getLogger(__name__)
 
@@ -117,7 +118,7 @@ class PagesAPIDetailView(APIDetailView):
 
     URL example: /api/pages/detail/1.json
     """
-    endpoint_class = PagesAPIEndpoint
+    endpoint_class = PagesAPIViewSet
 
     def get_build_path(self, page):
         return 'api/pages/detail/{pk}.json'.format(pk=page.pk)
@@ -148,7 +149,7 @@ class PagesAPIListingView(APIListingView):
         request = self.create_request(url)
         request.site = Site.objects.get(is_default_site=True)
         request.wagtailapi_router = WagtailAPIRouter('')
-        response = PagesAPIEndpoint.as_view({'get': 'listing_view'})(request)
+        response = PagesAPIViewSet.as_view({'get': 'listing_view'})(request)
 
         if response.status_code == 200:
             content = response.render().content
