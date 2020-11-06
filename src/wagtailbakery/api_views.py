ef064484c6e6512e664ceda4ae9e2aa9148f83a5
Loic Teixeira <hello@lta.me>
2020-11-06 09:27:48 +0100

    Remove old compatibility code
    Missing from #60

--- src/wagtailbakery/api_views.py
+++ src/wagtailbakery/api_views.py
@@ -5,11 +5,9 @@ import os
 from bakery.views import BuildableMixin
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
-from wagtail import VERSION as WAGTAIL_VERSION
+from wagtail.api.v2.endpoints import PagesAPIEndpoint
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.core.models import Page, Site
-
-from wagtailbakery.compatibility import PagesAPIViewSet
 
 logger = logging.getLogger(__name__)
 
@@ -102,8 +100,6 @@ class APIDetailView(BuildableMixin):
     def get_content(self, obj):
         # Create a dummy request
         request = self.create_request('/?format=json&fields=*')
-        if WAGTAIL_VERSION < (2, 9):
-            request.site = Site.objects.get(is_default_site=True)
         request.wagtailapi_router = WagtailAPIRouter('')
 
         response = self.endpoint_class.as_view({'get': 'detail_view'})(request, pk=obj.pk)
@@ -120,7 +116,7 @@ class PagesAPIDetailView(APIDetailView):
 
     URL example: /api/pages/detail/1.json
     """
-    endpoint_class = PagesAPIViewSet
+    endpoint_class = PagesAPIEndpoint
 
     def get_build_path(self, page):
         return 'api/pages/detail/{pk}.json'.format(pk=page.pk)
@@ -149,10 +145,8 @@ class PagesAPIListingView(APIListingView):
             url = '/?format=json&fields=*&limit={}&offset={}'.format(self.results_per_page, self.results_per_page * page_num)
 
         request = self.create_request(url)
-        if WAGTAIL_VERSION < (2, 9):
-            request.site = Site.objects.get(is_default_site=True)
         request.wagtailapi_router = WagtailAPIRouter('')
-        response = PagesAPIViewSet.as_view({'get': 'listing_view'})(request)
+        response = PagesAPIEndpoint.as_view({'get': 'listing_view'})(request)
 
         if response.status_code == 200:
             content = response.render().content
