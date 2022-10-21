527306c9dabef764278752f8f2063ea0c93c9e65
Storm Heg <storm@stormbase.digital>
2022-10-21 13:34:04 +0200

    Fix urlpatterns import for Django 4.0+

--- tests/urls.py
+++ tests/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 
 urlpatterns = [
-    url(r'^admin/', include(wagtailadmin_urls)),
-    url(r'', include(wagtail_urls)),
+    path('admin/', include(wagtailadmin_urls)),
+    path('', include(wagtail_urls)),
 ]
